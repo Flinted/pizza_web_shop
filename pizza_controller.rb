452622ef110d0a018f7ae848ebeb72a70de093bc
Chris Flint <flintedc@gmail.com>
2016-06-28 12:01:54 +0100

    can display pizzas

--- pizza_controller.rb
+++ pizza_controller.rb
@@ -1,3 +1,21 @@
 require ('sinatra')
 require ('sinatra/contrib/all')
 require ('pry-byebug')
+require_relative('./models/pizza')
+
+get '/pizza/new' do
+  erb(:new)
+end
+
+post '/pizza' do
+  #create pizza object
+  @pizza = Pizza.new(params)
+  #save to db
+  @pizza.save()
+  erb(:create)
+end
+
+get '/pizza' do
+  @pizzas = Pizza.all()
+  erb(:index)
+end
